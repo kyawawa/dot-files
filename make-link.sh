c27f1aed28c2f1933bf3d58e99f1f62d4f5241a6
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2015-10-20 12:49:45 +0900

    Fix for other directory name

--- make-link.sh
+++ make-link.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+dir=`pwd`
+
 for dotfile in .bashrc .emacs .emacs.d .gitconfig
 do
     if [ -e $HOME/$dotfile ]; then
@@ -8,13 +10,13 @@ do
 done
 
 if [ $# -eq 0 ]; then
-    ln -s $HOME/dot-files/.bashrc $HOME/.bashrc
+    ln -s $dir/.bashrc $HOME/.bashrc
 elif [ $# -eq 1 -a $1 = "JSK" ]; then
-    ln -s $HOME/dot-files/jsk.bash $HOME/.bashrc
+    ln -s $dir/jsk.bash $HOME/.bashrc
 else
     echo "Argument Error!!" 1>&2
     echo "Please select JSK or Nothing!!" 1>&2
 fi
 
-ln -s $HOME/dot-files/.emacs.d $HOME/ # not to create link such as $HOME/.emacs.d/.emacs.d
-ln -s $HOME/dot-files/.gitconfig $HOME/.gitconfig
+ln -s $dir/.emacs.d $HOME/ # not to create link such as $HOME/.emacs.d/.emacs.d
+ln -s $dir/.gitconfig $HOME/.gitconfig
