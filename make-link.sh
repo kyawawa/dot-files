9bf6d4773e672505153476ba00a0408ec77e3aef
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2015-11-07 12:36:12 +0900

    Add gtk.css

--- make-link.sh
+++ make-link.sh
@@ -9,6 +9,10 @@ do
     fi
 done
 
+if [ -e $HOME/.config/gtk-3.0/gtk.css ]; then
+    rm -ri $HOME/.config/gtk-3.0/gtk.css
+fi
+
 if [ $# -eq 0 ]; then
     ln -s $dir/.bashrc $HOME/.bashrc
 elif [ $# -eq 1 -a $1 = "JSK" ]; then
@@ -20,3 +24,4 @@ fi
 
 ln -s $dir/.emacs.d $HOME/ # not to create link such as $HOME/.emacs.d/.emacs.d
 ln -s $dir/.gitconfig $HOME/.gitconfig
+ln -s $dir/gtk.css $HOME/.config/gtk-3.0/gtk.css
