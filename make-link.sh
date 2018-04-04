864fd4a47005606fa8b040f7f32855de7ba5a3f8
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2018-04-04 11:28:51 -0400

    Add .gdbinit

--- make-link.sh
+++ make-link.sh
@@ -2,7 +2,7 @@
 
 dir=$(dirname $(readlink -f $0))
 
-for dotfile in .bashrc .emacs .emacs.d .gitconfig .globalrc
+for dotfile in .bashrc .emacs .emacs.d .gitconfig .globalrc .gdbinit .pythonstartup .ipython
 do
     if [ -e $HOME/$dotfile ]; then
         rm -ri $HOME/$dotfile
@@ -33,6 +33,7 @@ fi
 ln -sn $dir/.emacs.d $HOME/.emacs.d # not to create link such as $HOME/.emacs.d/.emacs.d
 ln -sn $dir/.gitconfig $HOME/.gitconfig
 ln -sn $dir/.globalrc $HOME/.globalrc
+ln -sn $dir/.gdbinit $HOME/.gdbinit
 ln -sn $dir/gtk.css $HOME/.config/gtk-3.0/gtk.css
 ln -sn $dir/.pythonstartup $HOME/.pythonstartup
 ln -sn $dir/.ipython $HOME/.ipython
