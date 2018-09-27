334cba64c31399add8b01bbe0306f7a04775de6e
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2018-09-27 23:45:21 +0900

    [make-link.sh] rm: Avoid begin asked at each level

--- make-link.sh
+++ make-link.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
+promptRemove () {
+    local yn
+    read -p "rm: remove '$1'? [Y/n] " yn
+    case $yn in
+        "Y" | "y" ) rm -rf $HOME/$1;;
+        * )
+    esac
+}
+
+
 dir=$(dirname $(readlink -f $0))
 
 for dotfile in .bashrc .emacs .emacs.d .gitconfig .globalrc .gdbinit .pythonstartup .ipython
 do
     if [ -e $HOME/$dotfile ]; then
-        rm -ri $HOME/$dotfile
+        promptRemove $dotfile
     fi
 done
 
 if [ -e $HOME/.config/gtk-3.0/gtk.css ]; then
-    rm -ri $HOME/.config/gtk-3.0/gtk.css
+    promptRemove $HOME/.config/gtk-3.0/gtk.css
 fi
 
 # .config
 for i in $(ls $dir/.config); do
-    if [ -e $HOME/.config/$i ]; then
-        rm -ri $HOME/.config/$i
+    home_conf_file=$HOME/.config/$i
+    if [ -e $home_conf_file ]; then
+        promptRemove $home_conf_file
     fi
-    ln -sn $dir/.config/$i $HOME/.config/$i
+    ln -sn $dir/.config/$i $home_conf_file
 done
 
 if [ $# -eq 0 ]; then
